daa274204bea0b0aebb6035007725b7653216da3
Romain Duquesne <romain.duquesne@icloud.com>
2021-03-30 11:29:06 +0200

    second

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,93 @@
+import * as dotenv from 'dotenv';
+import * as winston from 'winston';
+
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
-import {User} from "./entity/User";
+import {createConnection, Connection} from "typeorm";
 
-createConnection().then(async connection => {
+import * as express from 'express';
 
-    // create express app
-    const app = express();
-    app.use(bodyParser.json());
+import {DbHandler} from "./handlers/db.handler";
+import {AuthHandler} from "./handlers/auth.handler";
+import {ErrorHandler} from "./handlers/error.handler";
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+import { SessionController } from "./controllers/session.controller";
+import {ContactController} from "./controllers/contact.controller";
 
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
+dotenv.config({
+    path: `${__dirname}/env/${process.env.NODE_ENV ? process.env.NODE_ENV : 'development'}.env`
+});
+
+export class Server {
+    private readonly app: express.Application;
+
+    constructor() {
+        this.app = express();
+
+        this.loadLogger();
+
+        this.loadDb()
+            .then(() => this.loadApp())
+            .catch(err => console.error(err));
+    }
+
+    private loadLogger(): void {
+        this.app.locals.logger = winston.createLogger({
+            level: 'info',
+            format: winston.format.json(),
+            defaultMeta: { service: 'user-service' },
+            transports: [
+                new winston.transports.File({ filename: `${__dirname}/../logs/error.log`, level: 'error' }),
+                new winston.transports.File({ filename: `${__dirname}/../logs/combined.log` })
+            ],
         });
-    });
 
-    // setup express app here
-    // ...
+        if (process.env.NODE_ENV !== 'production') this.app.locals.logger.add(new winston.transports.Console({ format: winston.format.simple() }));
+    }
 
-    // start express server
-    app.listen(3000);
+    private loadDb(): Promise<void> {
+        return createConnection().then((connection: Connection) => {
+            this.app.locals.db = connection;
 
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
+            return this.app.locals.db.synchronize();
+        });
+    }
 
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
+    private loadMiddlewares(): void {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
 
-}).catch(error => console.log(error));
+        this.app.use(`${process.env.API_BASE_PATH}/static`, express.static(__dirname + '/../dist/public'));
+
+        this.app.use(new DbHandler(this.app.locals.db).loader);
+        this.app.use(new AuthHandler().loader);
+    }
+
+    private loadControllers(): void {
+        const sessionController = new SessionController();
+
+        this.app.post(`${process.env.API_BASE_PATH}/session/signin`, sessionController.signin);
+        this.app.post(`${process.env.API_BASE_PATH}/session/signup`, sessionController.signup);
+        this.app.get(`${process.env.API_BASE_PATH}/session/auth`, sessionController.getAuth);
+
+        const contactController = new ContactController();
+
+        this.app.get(`${process.env.API_BASE_PATH}/contacts`, contactController.gets);
+        this.app.post(`${process.env.API_BASE_PATH}/contacts`, contactController.add);
+    }
+
+    private loadApp(): void {
+        this.loadMiddlewares();
+        this.loadControllers();
+    }
+
+    launch(): void {
+        this.app.use(new ErrorHandler().loader);
+
+        this.app.listen(process.env.PORT, () => {
+            console.log(`MAD SERVER listen on ${process.env.PORT}.`)
+        });
+    }
+}
+
+new Server()
+    .launch();
